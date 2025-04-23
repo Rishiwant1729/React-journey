5ccc9e304383a85d0f67054f71ee60a046dcb930
RIshiwant Kumar Maurya <rishiwamtk143@gmail.com>
2025-04-24 00:59:59 +0530

    bg changer

---
+++ bgchanger/src/App.jsx
@@ -0,0 +1,123 @@
+import { useState } from "react"
+
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+    <div className= "w-full h-screen duration-200"
+    style={{ backgroundColor: color }}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+          onClick={() => setColor("red")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{ backgroundColor: "red" }}
+          >Red</button>
+
+          <button
+          onClick={() => setColor("green")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{ backgroundColor: "Green" }}
+          >Green</button>
+
+          <button
+          onClick={() => setColor("Blue")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{ backgroundColor: "Blue" }}
+          >Blue</button>
+
+          <button
+          onClick={() => setColor("brown")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{ backgroundColor: "brown" }}
+          >brown</button>
+
+
+          <button
+          onClick={() => setColor("yellow")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{ backgroundColor: "Yellow" }}
+          >Yellow</button>
+
+
+          <button
+          onClick={() => setColor("violet")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{ backgroundColor: "Violet" }}
+          >Violet</button>
+
+          <button
+          onClick={() => setColor("CornflowerBlue")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{ backgroundColor: "CornflowerBlue" }}
+          >CornflowerBlue</button>
+
+
+          <button
+          onClick={() => setColor("Coral")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{ backgroundColor: "Coral" }}
+          >Coral</button>
+
+
+          <button
+          onClick={() => setColor("Crimson")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{ backgroundColor: "Crimson" }}
+          >Crimson</button>
+
+
+          <button
+          onClick={() => setColor("Cornsilk")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{ backgroundColor: "Cornsilk" }}
+          >Cornsilk</button>
+
+
+          <button
+          onClick={() => setColor("DarkMagenta")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{ backgroundColor: "DarkMagenta" }}
+          >Magenta</button>
+
+
+          <button
+          onClick={() => setColor("DarkSeaGreen")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{ backgroundColor: "DarkSeaGreen" }}
+          >SeaGreen</button>
+
+          <button
+          onClick={() => setColor("DeepPink")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{ backgroundColor: "DeepPink" }}
+          >Pink</button>
+
+
+          <button
+          onClick={() => setColor("DimGrey")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{ backgroundColor: "DimGrey" }}
+          >Grey</button>
+
+          <button
+          onClick={() => setColor("OrangeRed")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{ backgroundColor: "OrangeRed" }}
+          >OrangeRed</button>
+
+          <button
+          onClick={() => setColor("PeachPuff")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{ backgroundColor: "PeachPuff" }}
+          >PeachPuff</button>
+
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
