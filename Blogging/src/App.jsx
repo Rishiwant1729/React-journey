399a7b17c712e4219240b9ac8bd82891116b2b37
RIshiwant Kumar Maurya <rishiwamtk143@gmail.com>
2025-05-22 13:45:24 +0530

    abhi tak badhiya hai

---
+++ Blogging/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl text-pink-300 text-center bg-blue-400 mt-2'>Blog app with full function</h1>
+    </>
+  )
+}
+
+export default App
