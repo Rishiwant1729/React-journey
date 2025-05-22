70d32c0a44a350323cfea1ebfa2f59d4c8b23438
RIshiwant Kumar Maurya <rishiwamtk143@gmail.com>
2025-05-22 14:56:14 +0530

    auth part complete kar liya

--- Blogging/src/App.jsx
+++ Blogging/src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
       <h1 className='text-3xl text-pink-300 text-center bg-blue-400 mt-2'>Blog app with full function</h1>
